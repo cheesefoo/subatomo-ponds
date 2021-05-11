60cc20b93986040ad7f3f1522f672ef61c72c6b5
cheesefoo <cheesefoo666@gmail.com>
2021-05-11 16:02:44 -0400

    add accessories to editor

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const app = express();
-const DIST_DIR = path.join(__dirname, '/dist');
-const HTML_FILE = path.join(DIST_DIR, 'index.html');
+const DIST_DIR = path.join(__dirname, "/dist");
+const HTML_FILE = path.join(DIST_DIR, "index.html");
 
 app.use(express.static(DIST_DIR));
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(HTML_FILE);
 });
 
