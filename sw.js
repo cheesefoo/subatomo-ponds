a2c80244229fd60d27a0d87240409153cced6e37
FranTRf <frantrf@gmail.com>
2021-06-30 21:40:12 -0300

    service worker

--- sw.js
+++ sw.js
@@ -1,23 +1,34 @@
-var cacheName = "shuba-pond";
-var filesToCache = [
-    "/",
-    "/index.html"
+const filesToCache = [
+  '/'
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener("install", function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+const staticCacheName = 'subatomo-ponds';
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-/* Serve cached content when offline */
-self.addEventListener("fetch", function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
+
+self.addEventListener('fetch', function(event) {
+    var request = event.request;
+    // console.log(request.url);
+	//request.url.indexOf('https://fonts.gstatic.com/')
+    if (true) {
+      event.respondWith(
+        caches.open('haceclick_dinamic').then(function(cache) {
+          return cache.match(event.request).then(function (response) {
+            return response || fetch(event.request).then(function(response) {
+              cache.put(event.request, response.clone());
+              return response;
+            });
+          });
         })
-    );
+      );
+    }
 });
